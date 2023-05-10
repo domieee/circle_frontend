7fdae56dd40b16965492973fc560ab153084dd78
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-10 23:05:22 +0200

    fix: prettier

--- app/auth/components/Login.js
+++ app/auth/components/Login.js
@@ -1,78 +1,77 @@
-import { View, TextInput, StyleSheet, Text, Button, TouchableOpacity } from 'react-native'
-import { useState } from 'react'
+import { View, TextInput, StyleSheet, Text, Button, TouchableOpacity } from 'react-native';
+import { useState } from 'react';
 import { useRouter } from 'expo-router';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Login = ({ setProcess }) => {
-
-    const [errorMsg, setErrorMsg] = useState('')
+    const [errorMsg, setErrorMsg] = useState('');
     const [mail, setMail] = useState('');
     const [password, setPassword] = useState('');
 
-    const router = useRouter()
+    const router = useRouter();
 
     const sendLoginData = async () => {
         try {
             const response = await fetch('https://circle-backend-2-s-guettner.vercel.app/api/v1/login', {
                 method: 'POST',
                 headers: {
-                    "Content-Type": "application/json"
+                    'Content-Type': 'application/json',
                 },
                 body: JSON.stringify({
                     mail: mail,
                     password: password,
-                })
-            })
+                }),
+            });
             if (response.ok) {
-                const userID = await response.json()
+                const userID = await response.json();
                 await AsyncStorage.setItem('userID', userID);
-                router.push('/home/feed')
+                router.push('/home/feed');
             } else if (response.statusCode === 400) {
-                const error = await response.json()
-                setErrorMsg(error.msg)
+                const error = await response.json();
+                setErrorMsg(error.msg);
             }
         } catch (err) {
-            console.log(err, errorMsg)
+            console.log(err, errorMsg);
         }
-    }
+    };
 
     return (
         <View style={styles.loginForm}>
             <Text>Login</Text>
             <TextInput
                 onChangeText={(e) => setMail(e)}
-                keyboardType='email-address'
+                keyboardType="email-address"
                 style={styles.input}
                 editable
-                placeholder='johndoe@mail.com'
-                placeholderTextColor="#808080" />
+                placeholder="johndoe@mail.com"
+                placeholderTextColor="#808080"
+            />
 
             <TextInput
                 onChangeText={(e) => setPassword(e)}
                 secureTextEntry={true}
                 style={styles.input}
                 editable
-                placeholder='Password'
-                placeholderTextColor="#808080" />
+                placeholder="Password"
+                placeholderTextColor="#808080"
+            />
 
-            <Button onPress={() => sendLoginData()} title='Login' />
+            <Button onPress={() => sendLoginData()} title="Login" />
 
-            <Text>
-                Not a member?
-            </Text>
+            <Text>Not a member?</Text>
 
             <TouchableOpacity onPress={() => setProcess('register')} style={styles.appButtonContainer}>
                 <Text style={styles.login}>Create Account</Text>
             </TouchableOpacity>
         </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
     loginForm: {
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     input: {
         width: 300,
@@ -82,8 +81,8 @@ const styles = StyleSheet.create({
         padding: 10,
     },
     login: {
-        fontSize: 10
-    }
-})
+        fontSize: 10,
+    },
+});
 
 export default Login;
