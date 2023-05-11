2e01b8fbe647d696da73b347ad002a97aae6eab0
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-11 09:55:53 +0200

    feat: added NavBar

--- app/home/_layout.js
+++ app/home/_layout.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Button, Text, Image, StyleSheet, StatusBar } from 'react-native';
 import { useState } from 'react';
+import { Ionicons } from '@expo/vector-icons';
 import Feed from '../home/feed.js';
 import Search from './search.js';
 import Upload from './upload.js';
@@ -15,23 +16,58 @@ const [statusBarTransition, setStatusBarTransition] = useState('fade');
 export default () => {
     return (
         <>
+            <Tab.Navigator
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                        let iconName;
 
-            <Tab.Navigator>
+                        if (route.name === 'Home') {
+                            iconName = focused ? 'ios-home' : 'ios-home-outline';
+                        } else if (route.name === 'Suche') {
+                            iconName = focused ? 'ios-search' : 'ios-search-outline';
+                        } else if (route.name === 'Upload') {
+                            iconName = focused ? 'ios-add-circle' : 'ios-add-circle-outline';
+                        } else if (route.name === 'Profile') {
+                            iconName = focused ? 'ios-person' : 'ios-person-outline';
+                        }
+
+                        return <Ionicons name={iconName} size={size} color={color} />;
+                    },
+                })}
+                tabBarOptions={{
+                    activeTintColor: 'tomato',
+                    inactiveTintColor: 'gray',
+                }}>
                 <Tab.Screen
+                    name="Home"
+                    component={Feed}
                     options={{
                         headerBackTitleVisible: false,
-                        headerLeft: () => (
-                            <Image
-                                style={styles.imageHeaderLeft}
-                                source={require('../assets/img/circle_logo.png')}
-                            />
-                        )
+                        headerLeft: () => <Image style={styles.imageHeaderLeft} source={require('../assets/img/circle_logo.png')} />,
+                        tabBarLabel: 'Home',
                     }}
-                    name="Feed" component={Feed} />
-                <Tab.Screen name="Suche" component={Search} />
-                <Tab.Screen name="Upload" component={Upload} />
-                <Tab.Screen name="Profile" component={Profile} />
-
+                />
+                <Tab.Screen
+                    name="Suche"
+                    component={Search}
+                    options={{
+                        tabBarLabel: 'Suche',
+                    }}
+                />
+                <Tab.Screen
+                    name="Upload"
+                    component={Upload}
+                    options={{
+                        tabBarLabel: 'Upload',
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={Profile}
+                    options={{
+                        tabBarLabel: 'Profile',
+                    }}
+                />
             </Tab.Navigator>
         </>
     );
