0e7a6ec303e61d9f5eb3439efd969673ee753f13
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-05-11 22:47:46 +0200

    feat

--- app/auth/components/PostLink.js
+++ app/auth/components/PostLink.js
@@ -1,16 +1,21 @@
 import { ScrollView, Text, StyleSheet,Image, StatusBar,View } from 'react-native'
 import React, { useEffect, useState ,useRef} from 'react'
-
+import { useNavigation } from '@react-navigation/native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 
 const PostLink = ({postImage,postId}) => {
 
-
+    const navigation = useNavigation()
    
     return (
         <>
-            <Image style={styles.postImage} source={{ uri: postImage }} />
+            <TouchableOpacity onPress={() => navigation.navigate('postDetails')}>
+            <Image 
+            style={styles.postImage} 
+            source={{ uri: postImage }} />
+            </TouchableOpacity>
 
         </>
     )
